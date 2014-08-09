25cfffbec0b0e24811a4217c063771d72c321b05
Mike McLin <mclinmike@gmail.com>
2014-08-09 16:04:06 -0400

    Create initial unit tests

--- test/unit/angular-responsive-elements_test.js
+++ test/unit/angular-responsive-elements_test.js
@@ -1,20 +1,51 @@
 'use strict';
 
 describe('ConfigService', function () {
+  var $scope;
+  var $compile;
   var $window;
   var RespondConfig;
+  var element;
 
   beforeEach(module('mm.responsiveElements'));
 
-  beforeEach(inject(function (_$window_, _RespondConfig_) {
-
+  beforeEach(inject(function (_$rootScope_, _$compile_, _$window_, _RespondConfig_) {
+    $scope = _$rootScope_;
+    $compile = _$compile_;
     $window = _$window_;
     RespondConfig = _RespondConfig_;
-
+    element = angular.element('<div respond></div>');
+    $compile(element)($scope);
   }));
 
-  it('should pass a test', function () {
-    expect(true).toBeTruthy();
+  describe('respond', function () {
+
+    describe('config', function () {
+
+      it('should use RespondConfig for configuration', function () {
+        expect(element.isolateScope().config).toMatch(RespondConfig);
+      });
+
+      it('should allow custom config per element', function () {
+        RespondConfig.start = 100;
+        expect(element.isolateScope().config.start).toMatch(100);
+        element = angular.element('<div respond respond-config="{start:300}"></div>');
+        $compile(element)($scope);
+        expect(element.isolateScope().config.start).toMatch(300);
+      });
+
+    });
+
+    describe('init()', function () {
+
+      it('should call renderBreakpointClasses()', function () {
+        var renderBreakpointClasses = spyOn(element.isolateScope(), 'renderBreakpointClasses');
+        element.isolateScope().init();
+        expect(renderBreakpointClasses).toHaveBeenCalled();
+      });
+
+    });
+
   });
 
 });
