1230fac8288c9ac3fd07786ba52e1424a3f0a542
Mike McLin <mclinmike@gmail.com>
2014-08-09 17:26:34 -0400

    Assign directive's isolate scope to a global var

--- test/unit/angular-responsive-elements_test.js
+++ test/unit/angular-responsive-elements_test.js
@@ -1,21 +1,23 @@
 'use strict';
 
 describe('ConfigService', function () {
-  var $scope;
+  var $rootScope;
   var $compile;
   var $window;
   var RespondConfig;
   var element;
+  var scope;
 
   beforeEach(module('mm.responsiveElements'));
 
   beforeEach(inject(function (_$rootScope_, _$compile_, _$window_, _RespondConfig_) {
-    $scope = _$rootScope_;
+    $rootScope = _$rootScope_;
     $compile = _$compile_;
     $window = _$window_;
     RespondConfig = _RespondConfig_;
     element = angular.element('<div respond></div>');
-    $compile(element)($scope);
+    $compile(element)($rootScope);
+    scope = element.isolateScope();
   }));
 
   describe('respond', function () {
@@ -23,15 +25,15 @@ describe('ConfigService', function () {
     describe('config', function () {
 
       it('should use RespondConfig for configuration', function () {
-        expect(element.isolateScope().config).toMatch(RespondConfig);
+        expect(scope.config).toMatch(RespondConfig);
       });
 
       it('should allow custom config per element', function () {
         RespondConfig.start = 100;
-        expect(element.isolateScope().config.start).toMatch(100);
+        expect(scope.config.start).toMatch(100);
         element = angular.element('<div respond respond-config="{start:300}"></div>');
-        $compile(element)($scope);
-        expect(element.isolateScope().config.start).toMatch(300);
+        $compile(element)($rootScope);
+        expect(scope.config.start).toMatch(300);
       });
 
     });
@@ -39,8 +41,8 @@ describe('ConfigService', function () {
     describe('init()', function () {
 
       it('should call renderBreakpointClasses()', function () {
-        var renderBreakpointClasses = spyOn(element.isolateScope(), 'renderBreakpointClasses');
-        element.isolateScope().init();
+        var renderBreakpointClasses = spyOn(scope, 'renderBreakpointClasses');
+        scope.init();
         expect(renderBreakpointClasses).toHaveBeenCalled();
       });
 
